17f5b7cfc44551b374c7151ed7af0102ad24b821
Abdulrasaq <darot@Abdulrasaqs-MacBook-Pro.local>
2022-05-15 20:22:02 +0100

    last query saved

--- WeatherApp/WeatherApp/UI/ViewModel/WeatherForecastViewModel.swift
+++ WeatherApp/WeatherApp/UI/ViewModel/WeatherForecastViewModel.swift
@@ -12,7 +12,8 @@ import Foundation
 import SwiftUI
 import RealmSwift
 final class WeatherForecastViewModel : ObservableObject {
-    @State private var lastQuery = UserDefaults.standard.string(forKey: Constant.LOCALITY)
+//    @State private var lastQuery = UserDefaults.standard.string(forKey: Constant.LOCALITY)
+    @AppStorage(Constant.LOCALITY) private var lastQuery = ""
     @Published var weatherResponse: WeatherResponse!
     @Published var stateData: UIModel = UIModel.loading
     @Published var locality = ""
@@ -28,6 +29,8 @@ final class WeatherForecastViewModel : ObservableObject {
     
     
     func getData(query:String, days:Int = 7){
+        print("`query` \(String(describing: query))")
+        UserDefaults.standard.set(query, forKey:  Constant.LOCALITY)
         dataManager.getLocalData(q: query, days: days) { [unowned self] weatherRealm in
             sendDataToView(weather: weatherRealm!)
         }
@@ -47,13 +50,22 @@ final class WeatherForecastViewModel : ObservableObject {
     
     func getDefaultLocation(){
         let lastQ = UserDefaults.standard.string(forKey: Constant.LOCALITY)
-        if lastQ == "" || lastQ == nil  {
-            print("getting default location")
-            dataManager.getDefaultLocation { [unowned self] localitu in
-                print("default location is \(localitu)")
+        print("lastQ \(String(describing: lastQuery))")
+        
+        dataManager.getDefaultLocation { [unowned self] localitu in
+            print("default location is \(localitu)")
+         
+            if lastQuery == ""  {
+                print("getting default location")
+                print("lastQ \(String(describing: lastQuery))")
                 getData(query: localitu)
             }
+            else{
+                print("real lastQ \(String(describing: lastQ))")
+                getData(query: lastQuery)
+            }
         }
+      
     }
     
     fileprivate func sendDataToView(weather: WeatherRealm){
